8fdac4eb15d3cf74ac1bdb20968ab56fcebef29f
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2025-03-19 11:51:38 +0000

    commit

--- src/config/config.production.ts
+++ src/config/config.production.ts
@@ -2,7 +2,8 @@ import baseConfig from "./config.base";
 
 const productionConfig = {
   ...baseConfig,
-  BASE_URL: "https://cherlygood.com",
+  BASE_URL: "https://cherlygood.vercel.app/",
+  // BASE_URL: "https://cherlygood.com",
   PAYPAL: {
     ...baseConfig.PAYPAL,
     API_BASE: "https://api-m.paypal.com",
