36d9742c540b8274645f1ea60bd4535f0ea6bf7b
Cherlygood <cherlygood@gmail.com>
2025-04-13 06:20:56 +0000

    commit

--- src/app/(website)/[slug]/page.tsx
+++ src/app/(website)/[slug]/page.tsx
@@ -64,7 +64,7 @@ export async function generateMetadata({
     title: product?.seo.metaTitle,
     description: product?.seo.metaDescription,
     alternates: {
-      canonical: `/${slug}-${productId}`,
+      canonical: `/${slug}`,
     },
   };
 }
