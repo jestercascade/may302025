98a3a6485368fccb4024760193b387c7956f4255
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2025-05-10 07:08:08 +0000

    commit

--- src/components/website/ProductDetails/StickyBar.tsx
+++ src/components/website/ProductDetails/StickyBar.tsx
@@ -188,7 +188,7 @@ export function StickyBar({
           {isInCart && (
             <button
               onClick={() => push("/cart")}
-              className="flex items-center justify-center w-max px-9 rounded-full cursor-pointer border border-[#c5c3c0] text-blue font-semibold h-[44px] shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#faf9f8_5%,_#eae8e6_100%)] bg-[#faf9f8] hover:[background:linear-gradient(to_bottom,_#eae8e6_5%,_#faf9f8_100%)] hover:bg-[#eae8e6] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)] min-[896px]:h-12"
+              className="flex items-center justify-center w-full md:max-w-60 rounded-full cursor-pointer border border-[#c5c3c0] text-blue font-semibold h-11 min-[896px]:h-12 shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#faf9f8_5%,_#eae8e6_100%)] bg-[#faf9f8] hover:[background:linear-gradient(to_bottom,_#eae8e6_5%,_#faf9f8_100%)] hover:bg-[#eae8e6] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)]"
             >
               In Cart - See Now
             </button>
