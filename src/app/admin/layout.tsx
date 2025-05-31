cb04096d0cac7a16b30dcaaf2ea440a27ea757e7
jestercascade <jestercascade@gmail.com>
2025-05-31 02:58:17 +0000

    commit

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,9 +13,9 @@ export default function AdminLayout({
       <main className="pt-24 pb-20 min-h-screen bg-neutral-50">
         <div className="w-full max-w-screen-lg mx-auto">{children}</div>
       </main>
-      <footer className="w-full py-5 border-t bg-neutral-50">
+      <footer className="w-full py-8 border-t bg-neutral-50">
         <div className="max-w-6xl mx-auto px-6">
-          <div className="flex justify-center text-xs text-gray-500">
+          <div className="flex justify-center text-xs text-gray">
             <p>© {new Date().getFullYear()} Cherlygood. All rights reserved.</p>
           </div>
         </div>
